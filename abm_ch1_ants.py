c0ebb5d70e7d4d318f1bee09925e6b5d4f9ae6a6
Mkmohamed1023 <mohamed.khalafala.hassan@gmail.com>
2025-10-01 15:11:50 +0100

    Update ant step function and config file

--- abm_ch1_ants.py
+++ abm_ch1_ants.py
@@ -1,4 +1,3 @@
 import sys, numpy, pandas, matplotlib, scipy, pyarrow, tqdm
 print(sys.executable)
 print(numpy.__version__, pandas.__version__, matplotlib.__version__, scipy.__version__, pyarrow.__version__, tqdm.__version__)
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
